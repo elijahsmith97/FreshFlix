283eb34e594812d36298f98a6c80b461319410ac
elijahsmith97 <smith.elijah97@gmail.com>
2020-08-15 19:21:00 -0400

    Adding movie description and release date to movies

--- FreshFlix/App.js
+++ FreshFlix/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, Image, View, ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native';
 import { render } from 'react-dom';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
@@ -18,7 +18,7 @@ export default class App extends React.Component {
 
   componentDidMount () {
     // TMDB API GET request with my API key
-    return fetch('https://api.themoviedb.org/3/movie/popular?api_key=bc44f90b35063a4410820f7ec8b8c469&language=en-US&page=2&region=US')
+    return fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=bc44f90b35063a4410820f7ec8b8c469&language=en-US')
       .then ( (response) => response.json() )
       .then ( (responseJson) => {
 
@@ -51,7 +51,9 @@ export default class App extends React.Component {
       let movie = this.state.dataSource.map((val, key) => {
         return <View key={key} style={styles.item}>
 
-          <Text>{val.original_title}</Text>
+          <Image style={styles.movieImage} source={{uri: 'https://image.tmdb.org/t/p/w185'}}></Image>
+          <Text style={styles.filmTitle}>{val.title} - {val.release_date}</Text>
+          <Text style={styles.overview}>{val.overview}</Text>
     
           </View>
       });
@@ -100,5 +102,11 @@ const styles = StyleSheet.create({
     fontFamily: 'Times New Roman',
     color: '#FF0000',
     marginTop: 40
+  },
+  filmTitle: {
+    fontWeight: 'bold'
+  },
+  overview: {
+    fontStyle: 'italic'
   }
 })
